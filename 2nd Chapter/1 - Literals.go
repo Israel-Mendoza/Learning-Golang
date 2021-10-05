1ee52695d3cb1b19cc2d4e3a87063f88765fee5e
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2021-10-05 01:59:37 -0500

    Fixed formatting

--- 2nd Chapter/1 - Literals.go
+++ 2nd Chapter/1 - Literals.go
@@ -4,13 +4,11 @@ import "fmt"
 
 func main() {
 
-	// Integer Literals
 	// Integer literals can be base ten, or other bases:
 	integerOne := 10
 	integerTwo := 0b1010110
 	integerThree := 0o75456
 	integerFour := 0xFA112C
-
 	fmt.Println(integerOne)
 	// 10
 	fmt.Println(integerTwo)
@@ -23,7 +21,6 @@ func main() {
 	// Floating point literals:
 	floatOne := 3.141598
 	floatTwo := 3.03e23
-
 	fmt.Println(floatOne)
 	// 3.141598
 	fmt.Println(floatTwo)
@@ -35,7 +32,6 @@ func main() {
 	runeThree := '\x61'
 	runeFour := '\u0061'
 	runeFive := '\U00000061'
-
 	fmt.Println(runeOne)
 	// 97
 	fmt.Println(runeTwo)
@@ -47,9 +43,9 @@ func main() {
 	fmt.Println(runeFive)
 	// 97
 
+	// String literals are delimited by double quotes and back ticks (for raw strings)
 	var stringOne string = "Hello there!"
 	var stringTwo string = `This is a rune: '\141'` // a raw string literal
-
 	fmt.Println(stringOne)
 	// Hello there!
 	fmt.Println(stringTwo)
@@ -58,7 +54,6 @@ func main() {
 	// Literals are untyped! They can be assigned to any compatible type:
 	aNUmber := 5           // Let's have 5's type be inferred
 	var aFloat float64 = 5 // 5 is assigned to a float64
-
 	fmt.Printf("%d - %T\n", aNUmber, aNUmber)
 	// 5 - int
 	fmt.Printf("%f - %T\n", aFloat, aFloat)
