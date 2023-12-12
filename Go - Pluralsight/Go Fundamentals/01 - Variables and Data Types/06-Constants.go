28c74b8f5328138c82ee8604e2c4a91a0827f880
Israel Mendoza <48306001+Israel-Mendoza@users.noreply.github.com>
2023-12-11 19:47:53 -0600

    Clearing repo

--- Go - Pluralsight/Go Fundamentals/01 - Variables and Data Types/06-Constants.go
+++ Go - Pluralsight/Go Fundamentals/01 - Variables and Data Types/06-Constants.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	const a = "Israel"       // Implicitly typed (will be treated as a literal)
 	const b string = "Mogom" // Explicitly typed (will be treated as a string)
@@ -25,4 +27,18 @@ func main() {
 		three // 3
 		four  // 4
 	)
+
+	const (
+		_  = iota
+		KB = float64(1 << (10 * iota))
+		MB
+		GB
+		TB
+	)
+
+	fmt.Printf("KB: %v\nMB: %v\nGB: %v\nTB: %v\n", KB, MB, GB, TB)
+	// KB: 1024
+	// MB: 1.048576e+06
+	// GB: 1.073741824e+09
+	// TB: 1.099511627776e+12
 }
---
+++ Go - Pluralsight/Go Fundamentals/01 - Variables and Data Types/07-Pointers.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		One thing we must understand is that when we declare a variable,
+		regardless of whether we assign it a value or not, the variable
+		in question will claim a memory address and won't let it go.
+		Even pointer variables work in this way.
+
+		Ex. The following variable declaration will give "aVariable" a space in memory,
+		which we cannot change once it's assigned.
+
+		var aVariable int
+
+		Think of a variable name as an alias for a memory address.
+	*/
+
+	anInt := 10
+	anIntPtr := &anInt // Gets the address of the anInt variable
+
+	fmt.Printf("Value: %v - Address: %v\n", anInt, anIntPtr)
+	// Value: 10 - Address: 0x140000a4018
+
+	// De-referencing a pointer:
+	aNumber := *anIntPtr   // Gets a copy of the value that anIntPtr is pointing to
+	aNumberPtr := &aNumber // Gets the address of the aNumber variable
+	fmt.Printf("Value: %v - Address: %v\n", aNumber, aNumberPtr)
+	// Value: 10 - Address: 0x140000a4028
+
+	// Manipulating the value of these variables:
+	anInt = 2500
+	fmt.Printf("Value: %v - Address: %v\n", anInt, anIntPtr)
+	// Value: 2500 - Address: 0x140000a4018
+	*anIntPtr = 3000
+	fmt.Printf("Value: %v - Address: %v\n", anInt, anIntPtr)
+	// Value: 3000 - Address: 0x140000a4018
+
+	aNumber = 99
+	fmt.Printf("Value: %v - Address: %v\n", aNumber, aNumberPtr)
+	// Value: 99 - Address: 0x140000a4028
+	*aNumberPtr = 999
+	fmt.Printf("Value: %v - Address: %v\n", aNumber, aNumberPtr)
+	// Value: 999 - Address: 0x140000a4028
+}
